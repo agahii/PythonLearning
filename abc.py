6d32cc3e1a9cb8dae0aefec2a5f335e8f8508244
Syed Ahmed Fraz Zaki <agahii@hotmail.com>
2023-02-16 21:01:32 +0500

    aaa

---
+++ abc.py
@@ -0,0 +1,16 @@
+import person
+
+c = person.mycal()
+
+
+
+f=[1,2,3,4]
+c.calSum()
+
+
+
+
+
+
+
+
---
+++ person.py
@@ -0,0 +1,11 @@
+class mycal:
+  
+  def calSum(self, va1,val2):
+    return va1+val2
+
+  def calSub(self, val1, val2):
+    return val1-val2
+
+    
+
+
