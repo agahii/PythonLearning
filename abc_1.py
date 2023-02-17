9d20c46e99410d255e67de7b6043f54e1725159d
Syed Ahmed Fraz Zaki <agahii@hotmail.com>
2023-02-17 21:58:20 +0500

    klhjkl

--- abc_1.py
+++ abc_1.py
@@ -1,4 +1,21 @@
 import myclass
 
+p1=myclass.person('fraz','8888','zaki', 100)
+p1.TellMeYourName()
+p1.depositFund(50)
+
+print(p1.bankAccountBalance)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- myclass.py
+++ myclass.py
@@ -1,10 +1,22 @@
 class person:
     name=''
     phone=''
+    fatherName=''
+    bankAccountBalance=0
 
-    def __init__(self,n,p):
+    def __init__(self,n,p, f, b):
         self.name=n
         self.phone=p
+        self.fatherName=f
+        self.bankAccountBalance=b
+        
+    def TellMeYourName(self):
+        print(self.name)
+
+    def depositFund(self, amount):
+        self.bankAccountBalance=self.bankAccountBalance+amount
+
+    
     
 
 
